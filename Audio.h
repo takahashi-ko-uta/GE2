f0a2ee828ac316e7a3bc22d4b29241b7d2b9d21f
KoRoTa-school@outlook.jp <KoRoTa-school@outlook.jp>
2023-04-26 15:58:25 +0900

    ファイルだけ追加

---
+++ Audio.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <xaudio2.h>
+#pragma comment(lib,"xaudio2.lib")
+
+#include <fstream>
+#include <wrl.h>
+
+using namespace Microsoft::WRL;
+
+class Audio
+{
+public:
+ 
+private:
+
+};
+
