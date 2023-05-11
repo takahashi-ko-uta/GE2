a388c318e0e4fbca0f0ec17d6862b160638eb56f
KoRoTa-school@outlook.jp <KoRoTa-school@outlook.jp>
2023-05-11 12:40:41 +0900

    とりあえずクラス化できた

--- Audio.h
+++ Audio.h
@@ -1,17 +1,60 @@
 #pragma once
 #include <xaudio2.h>
-#pragma comment(lib,"xaudio2.lib")
-
 #include <fstream>
 #include <wrl.h>
-
-using namespace Microsoft::WRL;
+#include <map>
+#include <string>
 
 class Audio
 {
 public:
- 
-private:
+    //チャンクヘッダ
+    struct ChunkHeader
+    {
+        char id[4];     //チャンク毎のID
+        int32_t size;   //チャンクサイズ
+    };
+    //RIFFヘッダチャンク
+    struct RiffHeader
+    {
+        ChunkHeader chunk;  //"RIFF"
+        char type[4];       //"WAVE"
+    };
+    //FMTチャンク
+    struct FormatChunk
+    {
+        ChunkHeader chunk;  //"fmt"
+        WAVEFORMATEX fmt;   //波形フォーマット
+    };
+    //音声データ
+    struct SoundData
+    {
+        //波形フォーマット
+        WAVEFORMATEX wfex;
+        //バッファの先頭アドレス
+        BYTE* pBuffer;
+        //バッファのサイズ
+        unsigned int bufferSize;
+    };
 
+    //初期化
+    void Initialize();
+    //データ解放
+    void Finalize();
+    //音声読み込み
+    void LoadWave(std::string filename);
+
+    /// <summary>
+    /// サウンドデータの解放
+    /// </summary>
+    void Unload(SoundData* soundData);
+
+    //音声再生
+    void PlayWave(std::string filename);
+private:
+    // XAudio2のインスタンス
+    Microsoft::WRL::ComPtr<IXAudio2> xAudio2_;
+    // サウンドデータの連想配列
+    std::map<std::string, SoundData> soundDatas_;
 };
 
--- main.cpp
+++ main.cpp
@@ -7,142 +7,20 @@
 #include "Model.h"
 #include "ImGuiManager.h"
 #include <imgui.h>
-#include <xaudio2.h>
-#pragma comment(lib,"xaudio2.lib")
 
 #include <fstream>
 
+#include "Audio.h"
 
 
-//チャンクヘッダ
-struct ChunkHeader
-{
-    char id[4];     //チャンク毎のID
-    int32_t size;   //チャンクサイズ
-};
-//RIFFヘッダチャンク
-struct RiffHeader
-{
-    ChunkHeader chunk;  //"RIFF"
-    char type[4];       //"WAVE"
-};
-//FMTチャンク
-struct FormatChunk
-{
-    ChunkHeader chunk;  //"fmt"
-    WAVEFORMATEX fmt;   //波形フォーマット
-};
-//音声データ
-struct SoundData
-{
-    //波形フォーマット
-    WAVEFORMATEX wfex;
-    //バッファの先頭アドレス
-    BYTE* pBuffer;
-    //バッファのサイズ
-    unsigned int bufferSize;
-};
 
-SoundData SoundLoadWave(const char* filename)
-{
-    //ファイル入力ストリームのインスタンス
-    std::ifstream file;
-    //.wavファイルをバイナリモードで開く
-    file.open(filename, std::ios_base::binary);
-    //ファイルオープン失敗を検出する
-    assert(file.is_open());
 
-    //RIFFヘッダの読み込み
-    RiffHeader riff;
-    file.read((char*)&riff, sizeof(riff));
-    //ファイルがRIFFかチェック
-    if (strncmp(riff.chunk.id, "RIFF", 4) != 0) {
-        assert(0);
-    }
-    //タイプがWAVEかチェック
-    if(strncmp(riff.type,"WAVE",4)!= 0){
-        assert(0);
-    }
-
-    //Formatチャンクの読み込み
-    FormatChunk format = {};
-    //チャンクヘッダーの確認
-    file.read((char*)&format, sizeof(ChunkHeader));
-    if (strncmp(format.chunk.id, "fmt ", 4) != 0) {//3だと動く
-        assert(0);
-    }
-    //チャンク本体の読み込み
-    assert(format.chunk.size <= sizeof(format.fmt));
-    file.read((char*)&format.fmt, format.chunk.size);
-
-    //Dataチャンクの読み込み
-    ChunkHeader data;
-    file.read((char*)&data, sizeof(data));
-    //JUNKチャンクを検出した場合
-    if(strncmp(data.id,"JUNK",4)== 0){
-        //読み取り位置をJUNKチャンクの終わりまで進める
-        file.seekg(data.size, std::ios_base::cur);
-        //再読み込み
-        file.read((char*)&data, sizeof(data));
-    }
-
-    if (strncmp(data.id, "data", 4) != 0) {
-        assert(0);
-    }
-
-    //Dataチャンクのデータ部（波形データ）の読み込み
-    char* pBuffer = new char[data.size];
-    file.read(pBuffer, data.size);
-
-    //Waveファイルを閉じる
-    file.close();
-
-    //returnする為の音声データ
-    SoundData soundData = {};
-
-    soundData.wfex = format.fmt;
-    soundData.pBuffer = reinterpret_cast<BYTE*>(pBuffer);
-    soundData.bufferSize = data.size;
-
-    return soundData;
-}
-
-void SoundUnload(SoundData* soundData)
-{
-    //バッファのメモリ
-    delete[] soundData->pBuffer;
-
-    soundData->pBuffer = 0;
-    soundData->bufferSize = 0;
-    soundData->wfex = {};
-}
-
-//音声再生
-void SoundPlayWave(IXAudio2* xAudio2, const SoundData& soundData) {
-    HRESULT result;
-
-    //波形フォーマットを元にSourceVoiceの生成
-    IXAudio2SourceVoice* pSourceVoice = nullptr;
-    result = xAudio2->CreateSourceVoice(&pSourceVoice, &soundData.wfex);
-    assert(SUCCEEDED(result));
-
-    //再生する波形データの設定
-    XAUDIO2_BUFFER buf{};
-    buf.pAudioData = soundData.pBuffer;
-    buf.AudioBytes = soundData.bufferSize;
-    buf.Flags = XAUDIO2_END_OF_STREAM;
-
-    //波形データの再生
-    result = pSourceVoice->SubmitSourceBuffer(&buf);
-    result = pSourceVoice->Start();
-}
 
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 #pragma region 基盤システム初期化
-    HRESULT result;
 
     //WindowsAPIの初期化
     WinApp* winApp = nullptr;
@@ -171,19 +49,16 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
     //object3dの初期化
     Object3d::StaticInitialize(dxCommon->GetDevice(), WinApp::window_width, WinApp::window_height);
     
+    //サウンドの初期化
+    Audio* audio = nullptr;
+    audio = new Audio();
+    audio->Initialize();
+
     //ImGuiManagerの初期化
     ImGuiManager* imGuiManager = nullptr;
     imGuiManager = new ImGuiManager;
     imGuiManager->Initialize(winApp, dxCommon);
 
-    //サウンドの変数宣言
-    Microsoft::WRL::ComPtr<IXAudio2> xAudio2;
-    IXAudio2MasteringVoice* masterVoice;
-    //xAudioエンジンのインスタンスを生成
-    result = XAudio2Create(&xAudio2, 0, XAUDIO2_DEFAULT_PROCESSOR);
-    //マスターボイスを生成
-    result = xAudio2->CreateMasteringVoice(&masterVoice);
-    
 #pragma endregion 基盤システム初期化
       
 #pragma region 最初のシーンの初期化
@@ -209,10 +84,12 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
     object3d_1->SetPosition({ 0,-5, 0 });
     object3d_2->SetPosition({ -5,0,-5 });
     object3d_3->SetPosition({ +5,0,+5 });
+
     //サウンドの読み込み
-    SoundData soundData1 = SoundLoadWave("Resources/Alarm01.wav");
+    audio->LoadWave("Resources/Alarm01.wav");
     //サウンド再生
-    SoundPlayWave(xAudio2.Get(), soundData1);
+    audio->PlayWave("Resources/Alarm01.wav");
+    //SoundPlayWave(xAudio2.Get(), soundData1);
 #pragma endregion 最初のシーンの初期化
     
 #pragma region ゲームループ
@@ -295,10 +172,11 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
     delete object3d_1;
     delete object3d_2;
     delete object3d_3;
-    //XAudioの解放
-    xAudio2.Reset();
-    //音声データの解放
-    SoundUnload(&soundData1);
+    //Audio解放
+    audio->Finalize();
+    delete audio;
+
+    
 #pragma endregion 基盤システムの終了
 
     return 0;
